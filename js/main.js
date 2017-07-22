254441189ceb65d836d49fe546b052fd2fba3a59
David <delivatorgaming@gmail.com>
2017-07-22 19:36:14 +0200

    Added 4 new commands

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 var consoleElement = document.getElementById("console"),
     consoleInput = document.getElementById("consoleInput"),
     linestart = document.getElementById("linestart"),
-    textToAdd = "";
-var messages = {
-  "welcome": "IF9fX19fICAgICAgIF8gXyAgICAgICAgICAgIF8gCnwgIF9fIFwgICAgIHwgKF8pICAgICAgICAgIHwgfAp8IHwgIHwgfCBfX198IHxfX18gICBfX19fIF98IHxfIF9fXyAgXyBfXyBfIF9fIF9fXyAgIF9fXyAKfCB8ICB8IHwvIF8gXCB8IFwgXCAvIC8gX2AgfCBfXy8gXyBcfCAnX198ICdfIGAgXyBcIC8gXyBcCnwgfF9ffCB8ICBfXy8gfCB8XCBWIC8gKF98IHwgfHwgKF8pIHwgfF8gfCB8IHwgfCB8IHwgIF9fLwp8X19fX18vIFxfX198X3xffCBcXy8gXF9fLF98XF9fXF9fXy98XyhfKXxffCB8X3wgfF98XF9fX3wKCgo="
-};
+    youtubePlayer = document.getElementById("youtube-player"),
+    textToAdd = "",
+    messages = {
+      "welcome": "IF9fX19fICAgICAgIF8gXyAgICAgICAgICAgIF8gCnwgIF9fIFwgICAgIHwgKF8pICAgICAgICAgIHwgfAp8IHwgIHwgfCBfX198IHxfX18gICBfX19fIF98IHxfIF9fXyAgXyBfXyBfIF9fIF9fXyAgIF9fXyAKfCB8ICB8IHwvIF8gXCB8IFwgXCAvIC8gX2AgfCBfXy8gXyBcfCAnX198ICdfIGAgXyBcIC8gXyBcCnwgfF9ffCB8ICBfXy8gfCB8XCBWIC8gKF98IHwgfHwgKF8pIHwgfF8gfCB8IHwgfCB8IHwgIF9fLwp8X19fX18vIFxfX198X3xffCBcXy8gXF9fLF98XF9fXF9fXy98XyhfKXxffCB8X3wgfF98XF9fX3wKCgo=",
+      "about": "QWJvdXQgbWU6CiAgTmFtZTogRGF2aWQgYWthIERlbGl2YXRvcgogIEFnZTogMTgKICBGcm9tOiBHZXJtYW55CiAgSG9iYmllczogcHJvZ3JhbW1pbmcsIHBsYXlpbmcgdmlkZW8gZ2FtZXMsIGV2ZXJ5dGhpbmcgdGhhdCBoYXMgdG8gZG8gd2l0aCBjb21wdXRlcnMsIGZpdG5lc3MKICBGYXZvdXJpdGUgZ2FtZXM6IENTOkdPLCBHVEEgVgogIFBDLVNwZWNzOgogICAgSW50ZXJuZXQ6IDEwME1iaXQvcyBkb3duLCA1TWJpdC9zIHVwCiAgICBNYWluYm9hcmQ6IE1TSSA5NzBBLUc0MwogICAgQ1BVOiBBTUQgRlgtNjMwMCAvIDYgQ29yZXMgQCA0R0h6CiAgICBTU0Q6IENydWNpYWwgTVgzMDAgMjU2R0IKICAgIEhERDogU29tZSBvbGQgV0QgMVRCIEhERAogICAgR1BVOiBBTUQgU2FwcGhpcmUgUlAgMjcwWAogICAgUGVyaXBoZXJhbHM6CiAgICAgIE1vdXNlOiBMb2dpdGVjaCBHNTAyIFByb3RldXMgU3BlY3RydW0KICAgICAgS2V5Ym9hcmQ6IExvZ2l0ZWNoIEc5MTAgT3Jpb24gU3BlY3RydW0KICAgICAgSGVhZHNldDogTG9naXRlY2ggRzQzMAogICAgICBNb3VzZXBhZDogUm9jY2F0IFRhaXRv"
+    };
 
 var encodedMessage = atob(messages.welcome);
 
@@ -18,6 +20,7 @@ function updateConsole() {
   } else {
     consoleElement.innerHTML += textToAdd[0];
     textToAdd = textToAdd.substr(1);
+    window.scrollTo(0, consoleElement.scrollHeight);
   }
 }
 
@@ -44,6 +47,11 @@ function executeCommand(command) {
   }
 
   switch (cmd) {
+    case "help":
+      if (args[0] == null) {
+        addText("Commands available: help, login, clear, reload, echo, goto, background, ifconfig, play, about, github\n");
+      }
+      break;
     case "login":
       if (!linestart.innerHTML.startsWith("anonymous")) {
         addText("Already logged in.\n");
@@ -92,6 +100,28 @@ function executeCommand(command) {
         addText("Your ip: " + ip.ip + "\n");
       });
       break;
+    case "play":
+      if (args[0] == null) {
+        addText("Usage: play <youtube-url>\n");
+      } else {
+        var videoId = args[0],
+            regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/,
+            match = videoId.match(regExp);
+        if (match && match[2].length == 11) {
+          videoId = match[2];
+          youtubePlayer.innerHTML = '<iframe width="0" height="0" src="http://www.youtube.com/embed/' + videoId + '?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+          addText("Embedding YouTube-Player with the ID: " + videoId + "\n");
+        } else {
+          addText("Invalid YouTube URL\n");
+        }
+      }
+      break;
+    case "about":
+      addText(atob(messages.about) + "\n")
+      break;
+    case "github":
+      consoleElement.innerHTML += 'Fork me on GitHub: <a href="https://github.com/Delivator/delivator.me">https://github.com/Delivator/delivator.me</a>\n';
+      break;
     default:
       if (command != "") {
         addText("Command not found. Use help for info.\n");
@@ -112,6 +142,7 @@ document.onkeypress = function(e) {
       consoleElement.innerHTML += linestart.innerHTML + consoleInput.value + "\n";
       executeCommand(command);
       consoleInput.value = "";
+      window.scrollTo(0, consoleElement.scrollHeight);
       break;
     default:
 
